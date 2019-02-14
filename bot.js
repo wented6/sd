1bcd7c811bdbf35e26b9c5f89a7709ed0ca48e94
wented6 <44713416+wented6@users.noreply.github.com>
2019-02-14 16:56:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,34 @@
-﻿
+const D1 = require('discord.js');
+
+
+
+
+const p1 = new D1.Client();
+
+
+
+
+
+
+    console.log("oNNN1");
+p1.on('guildMemberAdd', member => {
+const fuli= member.guild.channels.get("545257908334755852");
+if(!fuli) return;
+if(fuli) {
+setTimeout(() => fuli.send(`You're in **Fearless** Welcome **..**`), 4000)
+}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+p1.login(process.env.B);
