37062b9d9d9754b92efac8dd6f902f60d292eb86
wented6 <44713416+wented6@users.noreply.github.com>
2019-03-05 18:41:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,42 +1,15 @@
 const D1 = require('discord.js');
-const D2 = require('discord.js');
-
-
-
-
-
-const p1 = new D1.Client();
-const p2 = new D1.Client();
-
-
-
-
 
 
 
     console.log("oNNN1");
 p1.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("545257908334755852");
+const fuli= member.guild.channels.get("517077578515742781");
 if(!fuli) return;
 if(fuli) {
-setTimeout(() => fuli.send(`You're in **Bravery** Welcome **..**`), 4000)
+setTimeout(() => fuli.send(`Welcome To **Black Sky ..**`), 4000)
 }
 });
-
-
-
-
-    console.log("oNNN1");
-p2.on('guildMemberAdd', member => {
-const fuli= member.guild.channels.get("545257908334755852");
-if(!fuli) return;
-if(fuli) {
-setTimeout(() => fuli.send(`You're in **Bravery** Welcome **..**`), 3700)
-}
-});
-
-
-
 
 
 
@@ -48,4 +21,3 @@ setTimeout(() => fuli.send(`You're in **Bravery** Welcome **..**`), 3700)
 
 
 p1.login(process.env.B);
-p2.login(process.env.B2);
